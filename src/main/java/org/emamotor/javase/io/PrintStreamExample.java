edd759301c5cba7d736b9120c8f1b3beac7f7f1f
emag <lanabe.lanabe@gmail.com>
2013-11-24 09:37:31 +0900

    Add PrintStream test

---
+++ src/main/java/org/emamotor/javase/io/PrintStreamExample.java
@@ -0,0 +1,20 @@
+package org.emamotor.javase.io;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+public class PrintStreamExample {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2.3);
+        System.out.println('a');
+        System.out.println("test");
+
+        System.err.println(1);
+        System.err.println(2.3);
+        System.err.println('a');
+        System.err.println("test");
+    }
+
+}
